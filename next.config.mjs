c5a53a854bb62034cb8ac45040df53f43fe3bd5e
Gabriel372 <gabriel-dobt@hotmail.com>
2024-12-08 06:41:42 -0300

    update

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,19 @@
-/** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === "production";
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    unoptimized: true, // Disable default image optimization
+  },
+  assetPrefix: isProd ? "/falatu_frontend/" : "",
+  basePath: isProd ? "/falatu_frontend" : "",
+  output: "export",
 };
 
 export default nextConfig;
+
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+// };
+
+// export default nextConfig;
